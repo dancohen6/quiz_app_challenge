64bec4f5526ba8a96dae712a77c05a3b7b928bcd
dancohen6 <dzcsix@gmail.com>
2023-07-24 10:50:29 -0400

    added quiz questions and linked data.js in HTML

---
+++ data.js
@@ -0,0 +1,28 @@
+var quiz = [
+    {
+        question: 'In JavaScript, what is the result of 10 + "20"?',
+        choices:  ['1020', '30', 'Error', 'NaN'],
+        // Answer: A
+    },
+    {
+        question: 'Which built-in method in JavaScript is used to sort an array?',
+        choices:  ['sort()', 'order()', 'arrange()', 'sorted()'],
+        // Answer: A
+    },
+    {
+        question: 'What is the correct way to check if a variable is an array in JavaScript?',
+        choices: ['isArray(variable)', 'variable.isArray()', 'Array(variable)', 'typeof variable === "array"'],
+        // Answer: C
+    },
+    {
+        question: 'What does the "this" keyword refer to in JavaScript?',
+        choices: ['The current function', 'The global object', 'The object that owns the executing code', 'The most recent object created'],
+        // Answer: C
+    },
+    {
+        question: 'Which keyword is used to declare a variable in JavaScript?',
+        choices: ['variable', 'v', 'let', 'var'],
+        // Answer: D
+    }
+    
+]
